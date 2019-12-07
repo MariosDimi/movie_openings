3f3d8a123b6931289531ce45912d825bdae9ddd6
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2019-12-07 03:23:51 +0000

    modified cli.rb

--- lib/movie_openings/cli.rb
+++ lib/movie_openings/cli.rb
@@ -1,31 +1,28 @@
 class MovieOpenings::CLI 
   
   def call 
-    puts "Today's Movie Openings:"
     list_of_movies
-    movie_menu
+    menu
     closing
   end
   
   def list_of_movies
-    #here_doc https://www.rottentomatoes.com/browse/opening/
-    puts <<-DOC.gsub/^\s*/,''
+    puts <<-DOC
+      Welcome to the command line for new movies and information.
       1.PLAYMOBIL - 2019
       2.THE ISLAND - 2019
     DOC
-    @movies = MovieOpenings::Movies.today
-    @movies.each.with_index(1) do |movie, i|
-      puts "#{i}. #{movie.name} - #{movie.year} - #{movie.playing}"
-    end
+    @movies = MovieOpenings::Movies.all
   end
   
-  def movie_menu
-    input = nill
+  def menu
+    input = nil
     while input != "exit"
-    puts "Please choose if you want to know more information about those movies, or see them as list or simpy exit the menu."
+    puts "Please choose if you want to know more information about those movies by typing the corresponding name, or simply type 'exit' to close the menu.."
     input = gets.strip.downcase
+    case input
+    when "PLAYMOBIL"
       
-      if input.to_i > 0
         
         puts @movies[input.to_i-1]
       elsif input =="list"
